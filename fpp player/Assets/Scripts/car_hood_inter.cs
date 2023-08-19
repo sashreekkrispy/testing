b447a1e765a6e00873788a9a0b39a9a9ce7116da
Harshit5691 <117970365+Harshit5691@users.noreply.github.com>
2023-08-19 16:22:38 +0530

    for ansuman again and again

--- fpp player/Assets/Scripts/car_hood_inter.cs
+++ fpp player/Assets/Scripts/car_hood_inter.cs
@@ -5,13 +5,28 @@ using UnityEngine;
 public class car_hood_inter : MonoBehaviour
 {
     private Animator animator;
+    Camera cam;
+    RaycastHit hit;
+    Ray ray;
+    Collider Collidercol;
     private void Awake()
     {
         animator = GetComponent<Animator>();
-
+    }
+    private void Start()
+    {
+        Collidercol = GameObject.Find("hood.001").GetComponent<Collider>();
+        cam = GameObject.Find("Main Camera").GetComponent<Camera>();
     }
     private void Update()
     {
-        animator.SetBool("up", true);
+        ray = cam.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider == Collidercol)
+            {
+                animator.SetBool("up", true);
+            }
+        }
     }
 }
